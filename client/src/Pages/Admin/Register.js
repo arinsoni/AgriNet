6205ffd5d4e9e94ab35d783771acf6f902a5a401
Nandini Soni <117187906+iamnandini@users.noreply.github.com>
2024-02-11 10:37:08 +0530

    Register

--- client/src/Pages/Admin/Register.js
+++ client/src/Pages/Admin/Register.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/system";
 
 import { useFormik } from "formik";
-import { TextField, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const AdminRegister = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
@@ -31,77 +33,115 @@ const AdminRegister = () => {
     },
   });
 
-  const {
-    handleSubmit,
-    getFieldProps,
-    handleChange,
-    values,
-    handleBlur,
-  } = formik;
+  const { handleSubmit, getFieldProps, handleChange, values, handleBlur } =
+    formik;
+  const maxWidth = isMobile ? "90vw" : "600px";
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        {...getFieldProps("username")}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        id="username"
-        name="username"
-        value={values.username}
-        label="Username"
-        fullWidth
-        size="small"
-        color="primary"
-        variant="outlined"
-        margin="normal"
-        InputLabelProps={{
-          shrink: true
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <Paper
+        sx={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "row",
+          width: "80%",
+          maxWidth: maxWidth,
+          backgroundColor: "lightblue",
+          boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+          borderRadius: "10px",
+          padding: "10px",
         }}
-      />
-      <TextField
-        {...getFieldProps("password")}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        id="password"
-        name="password"
-        label="Password"
-        type="password"
-        fullWidth
-        size="small"
-        color="primary"
-        variant="outlined"
-        margin="normal"
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <TextField
-        {...getFieldProps("email")}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        id="email"
-        name="email"
-        value={values.email}
-        label="Email"
-        fullWidth
-        size="small"
-        color="primary"
-        variant="outlined"
-        margin="normal"
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        fullWidth
-        style={{ marginTop: "10px" }}
       >
-        Register
-      </Button>
-    </form>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            paddingRight: !isMobile ? "10px" : "",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <form onSubmit={handleSubmit}>
+              <TextField
+                {...getFieldProps("username")}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="username"
+                name="username"
+                value={values.username}
+                label="Username"
+                fullWidth
+                size="small"
+                color="primary"
+                variant="outlined"
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                {...getFieldProps("password")}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                fullWidth
+                size="small"
+                color="primary"
+                variant="outlined"
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                {...getFieldProps("email")}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                id="email"
+                name="email"
+                value={values.email}
+                label="Email"
+                fullWidth
+                size="small"
+                color="primary"
+                variant="outlined"
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                style={{ marginTop: "10px" }}
+              >
+                Register
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
