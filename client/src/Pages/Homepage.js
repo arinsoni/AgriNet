0807353d2a815d0a09821e0d162e9f1b7afd3299
Nandini Soni <soninandini982@gmail.com>
2024-02-11 23:31:02 +0530

    Navbar

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -7,6 +7,7 @@ import AdminLogin from "./Admin/Login";
 import FarmerRegister from "./Farmer/Register";
 import AdminRegister from "./Admin/Register";
 import AdminDash from "./AdminDashboard";
+import Navbar from "./Navbar";
 
 const Homepage = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -21,87 +22,89 @@ const Homepage = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-
   const maxWidth = isMobile ? "90vw" : "600px";
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        backgroundColor: "#f0f0f0",
-      }}
-    >
-      <Paper
+    <>
+      <Navbar />
+      <Box
         sx={{
           display: "flex",
-          flexDirection: isMobile ? "column" : "row",
-          width: "80%",
-          maxWidth: maxWidth,
-          backgroundColor: "lightblue",
-          boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
-          borderRadius: "10px",
-          padding: "10px",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          backgroundColor: "#f0f0f0",
         }}
       >
-        <Box
+        <Paper
           sx={{
-            width: "100%",
             display: "flex",
-            paddingRight: !isMobile ? "10px" : "",
-            justifyContent: "center",
-            alignItems: "center",
+            flexDirection: isMobile ? "column" : "row",
+            width: "80%",
+            maxWidth: maxWidth,
+            backgroundColor: "lightblue",
+            boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+            borderRadius: "10px",
+            padding: "10px",
           }}
         >
           <Box
             sx={{
+              width: "100%",
               display: "flex",
-              flexDirection: "column",
+              paddingRight: !isMobile ? "10px" : "",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
-            Log in as Admin
-            <AdminLogin />
-            <Typography variant="body2" color="textSecondary" mt={2}>
-              Not registered yet? Register{" "}
-              <Link to="/admin-register" style={{ color: "red" }}>
-                here
-              </Link>
-            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Log in as Admin
+              <AdminLogin />
+              <Typography variant="body2" color="textSecondary" mt={2}>
+                Not registered yet? Register{" "}
+                <Link to="/admin-register" style={{ color: "red" }}>
+                  here
+                </Link>
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+
           <Box
             sx={{
+              width: "100%",
               display: "flex",
-              flexDirection: "column",
               justifyContent: "center",
               alignItems: "center",
             }}
           >
-            Log in as Farmer
-            <FarmerLogin />
-            <Typography variant="body2" color="textSecondary" mt={2}>
-              Not registered yet? Register{" "}
-              <Link to="/farmer-register" style={{ color: "red" }}>
-                here
-              </Link>
-            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Log in as Farmer
+              <FarmerLogin />
+              <Typography variant="body2" color="textSecondary" mt={2}>
+                Not registered yet? Register{" "}
+                <Link to="/farmer-register" style={{ color: "red" }}>
+                  here
+                </Link>
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
-    </Box>
+        </Paper>
+      </Box>
+    </>
   );
 };
 
